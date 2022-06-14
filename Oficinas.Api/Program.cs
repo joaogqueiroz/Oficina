55a7e87745047af87134e6cdd52dbcd0d6f60cc5
João de Queiroz <joao.queiroz@modalgr.com.br>
2022-06-14 15:33:08 -0300

    feature: injetando dependencias, criando e adicionando connectionString

--- Oficinas.Api/Program.cs
+++ Oficinas.Api/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Oficinas.Core.Repostories;
+using Oficinas.Infrastructure.Persistence;
+using Oficinas.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("OficinaCs");
+builder.Services.AddDbContext<OficinaDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IOficinaRepository, OficinaRepository>();
+builder.Services.AddScoped<IServicoRepository, ServicoRepository>();
+builder.Services.AddScoped<IAgendamentoRepository, AgendamentoRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
