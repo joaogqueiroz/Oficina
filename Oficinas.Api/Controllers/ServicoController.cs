eb1d19fb2152d4d98fe48a8070f326daa4dc28c9
João de Queiroz <joao.queiroz@modalgr.com.br>
2022-06-15 21:50:12 -0300

    feature: criando servico

---
+++ Oficinas.Api/Controllers/ServicoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Oficinas.Application.Commands.CreateServico;
+using Oficinas.Application.Commands.UpdateServico;
+using Oficinas.Application.Queries.GetAllServico;
+using Oficinas.Application.Queries.GetServicoById;
+
+
+namespace Oficinas.Api.Controllers
+{
+    [Route("api/servico")]
+    [ApiController]
+    public class ServicoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+        [HttpGet("Id")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateServicoCommand command)
+        {
+            var id = 0;
+            return CreatedAtAction(nameof(GetById), new { id = id }, command);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, [FromBody] UpdateServicoCommand command)
+        {
+
+            return NoContent();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            return NoContent();
+        }
+    }
+}
