90ea5d0ceca04f0e0690eb1eb9fea925fb9b38f2
João de Queiroz <joao.queiroz@modalgr.com.br>
2022-06-17 04:03:40 -0300

    feature: validacoes basicas e validacao cnpj com regex

---
+++ Oficinas.Application/Validators/CreateAgendamentoCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Oficinas.Application.Commands.CreateAgendamento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficinas.Application.Validators
+{
+    public class CreateAgendamentoCommandValidator : AbstractValidator<CreateAgendamentoCommand>
+    {
+        public CreateAgendamentoCommandValidator()
+        {
+            RuleFor(a => a.IdServico)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Id do servico e obrigatorio!");
+
+            RuleFor(a => a.IdOficina)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Id da oficina e obrigatorio!");
+
+            RuleFor(a => a.DataAgendamento)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("data do agendamento e obrigatoria!");
+        }
+    }
+}
---
+++ Oficinas.Application/Validators/UpdateAgendamentoCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Oficinas.Application.Commands.UpdateAgendamento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficinas.Application.Validators
+{
+    public class UpdateAgendamentoCommandValidator : AbstractValidator<UpdateAgendamentoCommand>
+    {
+        public UpdateAgendamentoCommandValidator()
+        {
+
+        RuleFor(a => a.IdServico)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Id do servico e obrigatorio!");
+
+
+        RuleFor(a => a.DataAgendamento)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("data do agendamento e obrigatoria!");
+
+        }
+    }
+}
