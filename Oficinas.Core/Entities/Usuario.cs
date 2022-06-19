b25658c293e2b338d0f7a158b096c55e3e1fb618
João de Queiroz <joao.queiroz@modalgr.com.br>
2022-06-18 23:05:19 -0300

    feature: incluso autenticacao e criacao da entidade usuario

---
+++ Oficinas.Core/Entities/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using Oficinas.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficinas.Core.Entities
+{
+    public class Usuario : BaseEntity
+    {
+        public Usuario(string nomeCompleto, string email, DateOnly dataNascimento, DateOnly dataCriacao, string senha, RolesEnum role)
+        {
+            NomeCompleto = nomeCompleto;
+            Email = email;
+            DataNascimento = dataNascimento;
+            DataCriacao = dataCriacao;
+            Senha = senha;
+            Role = role;
+        }
+
+        public string NomeCompleto { get; private set; }
+        public string Email { get; private set; }
+        public DateOnly DataNascimento { get; private set; }
+        public DateOnly DataCriacao { get; private set; }
+        public string Senha { get; private set; }
+        public RolesEnum Role { get; private set; }
+    }
+}
